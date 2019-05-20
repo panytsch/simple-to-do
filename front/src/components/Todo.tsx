11622ade1b5ad9f1ab1b7e0095ceb700f2ba807a
Panasiuk <gfyroman@gmail.com>
2019-05-21 00:33:13 +0300

    delete and done are done :)

--- front/src/components/Todo.tsx
+++ front/src/components/Todo.tsx
@@ -5,11 +5,12 @@ import {Todo} from "../redux/structs"
 export const TodoComponent :React.FC = (props :any) => {
     const todo :Todo = props.Todo;
     return (
-        <div className="todo" key={todo.ID}>
+        <div className="todo" key={props.key}>
             <span>{todo.ID}</span>
-            <input type="checkbox" checked={todo.IsDone}/>
+            <input type="checkbox" checked={todo.IsDone} onChange={props.handlerDone}/>
             <span>{todo.Title}</span>
             <span>{todo.Description}</span>
+            <button onClick={props.handlerDelete}>Delete</button>
         </div>
     )
 };
--- front/src/redux/actions.tsx
+++ front/src/redux/actions.tsx
@@ -22,8 +22,26 @@ export const WsAddAction = (previousState :ReduxStateTodosInterface, action :Act
     return NewState;
 };
 
+export const WsUpdateAction = (previousState :ReduxStateTodosInterface, action :Action) :ReduxStateTodosInterface => {
+    const Helper = new WsReducerHelper(previousState.Todos);
+    let changedTodo :Todo = ((action.Payload as WsResponse).Todos as Todo[])[0] || null;
+    let NewState :ReduxStateTodosInterface = { Todos: [] };
+    NewState.Todos = Helper.changeTodo(changedTodo);
+    return NewState;
+
+};
+
+export const WsDeleteAction = (previousState :ReduxStateTodosInterface, action :Action) :ReduxStateTodosInterface => {
+    const Helper = new WsReducerHelper(previousState.Todos);
+    let todoToDelete :Todo = ((action.Payload as WsResponse).Todos as Todo[])[0] || null;
+    let NewState :ReduxStateTodosInterface = { Todos: [] };
+    NewState.Todos = Helper.deleteTodo(todoToDelete);
+    return NewState;
+
+};
+
 class WsReducerHelper {
-    private readonly Todos :Todo[];
+    private Todos :Todo[];
     constructor(todos :Todo[]){
         this.Todos = todos;
     }
@@ -33,5 +51,13 @@ class WsReducerHelper {
     }
     private getTodos() :Todo[] {
         return [...this.Todos]
+    }
+    public changeTodo(todoToChange :Todo) :Todo[] {
+        this.Todos = this.Todos.map(todo => todo.ID === todoToChange.ID ? todoToChange : todo);
+        return this.getTodos();
+    }
+    public deleteTodo(todoToDelete :Todo) :Todo[] {
+        this.Todos = this.Todos.filter(todo => todo.ID !== todoToDelete.ID);
+        return this.getTodos();
     }
 }
